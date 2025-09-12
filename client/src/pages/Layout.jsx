4a81183bee6e3278c851ef8b41d2d78093929793
kunwarxp <kunwaraditya0073@gmail.com>
2025-09-12 14:57:41 +0530

    Convert client folder into normal project folder

---
+++ client/src/pages/Layout.jsx
@@ -0,0 +1,11 @@
+import Rreact from "react";
+
+const LayOut = () => {
+    return (
+        <div>
+           <h1>Layout</h1>
+        </div>
+    )
+}
+
+export default LayOut;
---
+++ client/src/pages/Messages.jsx
@@ -0,0 +1,11 @@
+import react from 'react';
+
+const Messages = () => {
+    return(
+        <div>
+            <h1>Messages Page</h1>
+        </div>
+    )
+}
+
+export default Messages;
